c0629b057163e0ce1faa6e0568cc9085acc6eda8
MATTHIAS-PC\Matthias <matthias.schwegler@stud.hslu.ch>
2017-04-10 16:42:37 +0200

    Common Files Renamed project sucessfully

---
+++ Code/SmartBoard/Common/Application.c
@@ -0,0 +1,38 @@
+/*
+ * Application.c
+ *      Author: Erich Styger
+ */
+#include "Platform.h"
+#include "Application.h"
+#include "FRTOS1.h"
+#include "Shell.h"
+#include "LED1.h"
+#include "RNet_App.h"
+
+void APP_DebugPrint(uint8_t *str) {
+ /* dummy */
+}
+
+static void led_task(void *param) {
+  (void)param;
+  for(;;) {
+    LED1_Neg();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  } /* for */
+}
+
+void APP_Run(void) {
+#if PL_CONFIG_HAS_SHELL
+  SHELL_Init();
+#endif
+#if PL_CONFIG_HAS_RADIO
+  RNETA_Init();
+#endif
+#if PL_CONFIG_HAS_DUMMY_LED
+  if (xTaskCreate(led_task, "Led", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL) != pdPASS) {
+    for(;;){} /* error! probably out of memory! */
+  }
+#endif
+  vTaskStartScheduler();
+}
+
---
+++ Code/zum löschen/tinyK20_nrf/Sources/Shell.c
@@ -0,0 +1,54 @@
+/*
+ * Shell.c
+ *
+ *  Created on: 04.08.2011
+ *      Author: Erich Styger
+ */
+
+#include "Platform.h"
+#include "FRTOS1.h"
+#include "Shell.h"
+#include "CLS1.h"
+#include "Radio.h"
+#if PL_CONFIG_HAS_SEGGER_RTT
+  #include "RTT1.h"
+#endif
+#if PL_CONFIG_HAS_RADIO
+  #include "RNET1.h"
+  #include "RApp.h"
+  #include "RNet_App.h"
+  #include "RNetConf.h"
+#endif
+
+static const CLS1_ParseCommandCallback CmdParserTable[] =
+{
+  CLS1_ParseCommand,
+  FRTOS1_ParseCommand,
+#if PL_CONFIG_HAS_RADIO
+#if RNET1_PARSE_COMMAND_ENABLED
+  RNET1_ParseCommand,
+#endif
+#if PL_CONFIG_HAS_SHELL
+  RNETA_ParseCommand,
+#endif
+#endif
+  NULL /* sentinel */
+};
+
+static void ShellTask(void *pvParameters) {
+  unsigned char buf[48];
+  (void)pvParameters; /* not used */
+  buf[0] = '\0';
+  (void)CLS1_ParseWithCommandTable((unsigned char*)CLS1_CMD_HELP, CLS1_GetStdio(), CmdParserTable);
+  for(;;) {
+    (void)CLS1_ReadAndParseWithCommandTable(buf, sizeof(buf), CLS1_GetStdio(), CmdParserTable);
+    FRTOS1_vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
+void SHELL_Init(void) {
+  if (FRTOS1_xTaskCreate(ShellTask, "Shell", configMINIMAL_STACK_SIZE+300, NULL, tskIDLE_PRIORITY+1, NULL) != pdPASS) {
+    for(;;){} /* error */
+  }
+}
+
